977b3dcf0b76a9146d739ba38ebe951116d5f469
Evan Czaplicki <evancz@users.noreply.github.com>
2016-08-08 18:07:40 -0700

    Better errors for large record types
    Before, we just went until any field caused a problem. If it was an
    early field, there may be 10 more that all match, but have not been
    unified. The result was that the *looked* different in the type diff,
    but it was all visual noise.
    
    So this version unifies all fields no matter what. In simple cases,
    this makes the type errors much more specific. I can imagine it getting
    things “wrong” in more complex scenarios if a wrong field is taken as
    true, causing an error in a subsequent field. Then that gets cut out of
    the diff or something. Maybe should make a note about that possibility.

--- src/Reporting/Error/Type.hs
+++ src/Reporting/Error/Type.hs
@@ -36,7 +36,8 @@ data Mismatch = MismatchInfo
 
 
 data Reason
-    = MessyFields [String] [String]
+    = BadFields [String]
+    | MessyFields [String] [String]
     | IntFloat
     | TooLongComparableTuple Int
     | MissingArgs Int
@@ -594,6 +595,9 @@ funcName maybeVar =
 flipReason :: Reason -> Reason
 flipReason reason =
   case reason of
+    BadFields fields ->
+        BadFields fields
+
     MessyFields leftOnly rightOnly ->
         MessyFields rightOnly leftOnly
 
@@ -617,6 +621,12 @@ reasonToString reason =
       Just (toHint msg)
   in
   case reason of
+    BadFields [field] ->
+        go $ "It looks like a problem with the `" ++ field ++ "` field."
+
+    BadFields fields ->
+        go $ "I found problems in these fields: " ++ Help.commaSep fields
+
     MessyFields leftOnly rightOnly ->
         do  let typos = Help.findPotentialTypos leftOnly rightOnly
             _ <- Help.vetTypos typos
